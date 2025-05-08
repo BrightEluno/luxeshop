832f3e93e77ac1fb2a4485d50fb41b8a6aa27ea3
brightchinaza <brightochinaza96@gmail.com>
2025-05-08 19:10:39 +0100

    update

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        {/* <Text>Open up App.js to start working on your app!</Text> */}
+        <View style={{ flex: 1 }}>
+          <Image
+            source={require("./assets/logo.png")}
+            style={{ width: 50, height: 50 }}
+          />
+          <Text style={{ fontSize: 20, fontWeight: "bold", marginTop: 10 }}>
+            Luxeshop
+          </Text>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   // flex: 1,
+   backgroundColor: '#fff',
+   // alignItems: 'center',
+   padding: 20,
+   paddingTop: 50,
+   // justifyContent: 'center',
   },
 });
